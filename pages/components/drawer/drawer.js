12a7bb346bd79bcf8e5083a1635b2786f590fafa
ggao <ggao15@ford.com>
2019-09-16 18:37:17 +0800

    init project
    init drawer

---
+++ pages/components/drawer/drawer.js
@@ -0,0 +1,30 @@
+// pages/components/drawer/drawer.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    isShow: {
+      type: Boolean,
+      value: false
+    },
+    isEnter: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
